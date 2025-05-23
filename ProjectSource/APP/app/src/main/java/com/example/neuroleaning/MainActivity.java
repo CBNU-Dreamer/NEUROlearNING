038094a7759e92530963f9e4e9e92fa2f8fa7ff8
Damin <kimdm5112@gmail.com>
2025-05-23 11:50:06 +0900

    feat: page 5 초안 UI 구성 완료

--- ProjectSource/APP/app/src/main/java/com/example/neuroleaning/MainActivity.java
+++ ProjectSource/APP/app/src/main/java/com/example/neuroleaning/MainActivity.java
@@ -1,8 +1,8 @@
 package com.example.neuroleaning;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
